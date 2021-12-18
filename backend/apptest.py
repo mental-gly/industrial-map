e6734cc26cc8fe1c5fdc3e7df805ca11ca86e9b4
mental-gly <78332858+mental-gly@users.noreply.github.com>
2021-12-18 11:07:39 +0800

    上传测试用的后端文件

---
+++ backend/apptest.py
@@ -0,0 +1,17 @@
+# apptest.py
+from flask import Flask,render_template,request,make_response
+from flask_cors import CORS
+
+app = Flask(__name__,template_folder="templates",static_folder="static",static_url_path="/backend/static")
+CORS(app)
+
+@app.route('/',methods = ['POST','GET','OPTIONS'])
+def index():
+    return "1"
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    return "1"
+
+if __name__ == '__main__':
+    app.run('127.0.0.1', port=5000, debug=True)
